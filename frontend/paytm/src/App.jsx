ddc0cd0c6187d2628a58decf45d90f2821ee41d1
Mahesh Pawar <mp002995@gmail.com>
2024-10-16 14:45:56 +0530

    whitelist frontend url

--- frontend/paytm/src/App.jsx
+++ frontend/paytm/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <>
       <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard/>}/>
+        <Route path='/' element={<Signup/>}/>
         <Route path='/signup' element={<Signup/>}/>
         <Route path='/signin' element={<Signin/>}/>
         <Route path='/dashboard' element={<Dashboard/>}/>
