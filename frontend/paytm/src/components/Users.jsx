0546081a21b2036233ef77b5f2359bb064ee72a4
Mahesh Pawar <mp002995@gmail.com>
2025-04-27 22:34:57 +0530

    make changes in User component

--- frontend/paytm/src/components/Users.jsx
+++ frontend/paytm/src/components/Users.jsx
@@ -7,7 +7,8 @@ import { useNavigate } from 'react-router-dom';
 
 function Users() {
   const [users, setUsers] = useState([]);
-  const [filter, setFilter] = useState("")
+  const [filter, setFilter] = useState("");
+  const [focused, setFocused] = useState(false)
 
   useEffect(()=>{
     axios.get("https://paytm-backend-g7eh.onrender.com/api/v1/user/bulk?filter=" + filter)
@@ -16,16 +17,33 @@ function Users() {
       setUsers(response.data.user)
     })
   }, [filter])
+
+  const handleFocus =()=>{
+    setFocused(true)
+  }
+
   return (
     <>
       <div className="inline-block mt-4 bg-gray-500 text-white font-bold rounded-md py-2 px-4 hover:bg-gray-800 cursor-pointer">Send Money</div>
       <div className="my-2">
         <input onChange={e=>{
           setFilter(e.target.value)
-        }} type="text" placeholder="Search Users..." className="w-full px-2 py-1 border rounded border-slate-400" />
+        }} 
+        onFocus={handleFocus}
+        type="text" placeholder="Search Users..." className="w-full px-2 py-1 border rounded border-slate-400" />
       </div>
       <div>
-        {users.map(user=><User user={user}/>)}
+        {/* {
+          focused && (            
+              users.map(user=><User  user={user}/>)
+          )
+        } */}
+
+        {/* add height to user coponet  */}
+      <div className="h-[200px] overflow-auto" >
+        {focused &&
+          users.map((user) => <User key={user._id} user={user} />)}
+      </div>
       </div>
     </>
   );
@@ -34,7 +52,7 @@ function Users() {
 function User({user}){
   const navigate = useNavigate()
     return (
-    <div className="flex justify-between">
+    <div className="flex justify-between w-[98%]">
         <div className="flex">
             <div className="rounded-full h-12 w-12 bg-slate-400 flex justify-center mt-1 mr-2">
                 <div className="flex flex-col justify-center h-full text-xl">
